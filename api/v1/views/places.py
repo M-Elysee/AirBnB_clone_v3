3b7001cd8c426e20c233fe50662e40cccd721b4e
WillingtonM <willington.mhlanga@gmail.com>
2023-11-01 03:32:07 +0200

    Pycodestyle checks

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -17,12 +17,9 @@ from models import storage
 def get_places(city_id):
     """ Retrieves list of all Place objects of City object """
     cty = storage.get(City, city_id)
-
     if not cty:
         abort(404)
-
     plcs = [plc.to_dict() for plc in cty.places]
-
     return jsonify(plcs)
 
 
@@ -33,7 +30,6 @@ def get_place(place_id):
     plc = storage.get(Place, place_id)
     if not plc:
         abort(404)
-
     return jsonify(plc.to_dict())
 
 
@@ -43,25 +39,19 @@ def get_place(place_id):
 def post_place(city_id):
     """ Creates Place object """
     cty = storage.get(City, city_id)
-
     if not cty:
         abort(404)
-
     if not request.get_json():
         abort(400, description="Not a JSON")
-
     if 'user_id' not in request.get_json():
         abort(400, description="Missing user_id")
-
     dta = request.get_json()
     usr = storage.get(User, dta['user_id'])
-
     if not usr:
         abort(404)
 
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
-
     dta["city_id"] = city_id
     instance = Place(**dta)
     instance.save()
@@ -73,16 +63,33 @@ def post_place(city_id):
 @swag_from('documentation/place/delete_place.yml', methods=['DELETE'])
 def delete_place(place_id):
     """ Deletes a Place Object """
-
     plc = storage.get(Place, place_id)
-
     if not plc:
         abort(404)
-
     storage.delete(plc)
     storage.save()
+    return make_response
 
-    return make_response(jsonify({}), 200)
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/place/put_place.yml', methods=['PUT'])
+def put_place(place_id):
+    """ Updates Place object """
+    plc = storage.get(Place, place_id)
+    if not plc:
+        abort(404)
+    dta = request.get_json()
+    if not dta:
+        abort(400, description="Not a JSON")
+    ign = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+    for key, value in dta.items():
+        if key not in ign:
+            setattr(plc, key, value)
+    storage.save()
+    return make_response(jsonify(plc.to_dict()), 200)
+
+
+(jsonify({}), 200)
 
 
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
@@ -143,25 +150,3 @@ def places_search():
         plcs.append(d)
 
     return jsonify(plcs)
-
-
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/place/put_place.yml', methods=['PUT'])
-def put_place(place_id):
-    """ Updates Place object """
-    plc = storage.get(Place, place_id)
-
-    if not plc:
-        abort(404)
-
-    dta = request.get_json()
-    if not dta:
-        abort(400, description="Not a JSON")
-
-    ign = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-
-    for key, value in dta.items():
-        if key not in ign:
-            setattr(plc, key, value)
-    storage.save()
-    return make_response(jsonify(plc.to_dict()), 200)
